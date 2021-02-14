8255b5df608d31edee583fd8e11d178950761e9e
jpaetsch <j.paetsch@live.com>
2021-02-13 22:30:12 -0700

    Test version integration with controller working!  One known issue to look into, something to do with the timeout and buffer but otherwise looking to build from here

--- LEDsReceiver.ino
+++ LEDsReceiver.ino
@@ -9,12 +9,13 @@
 
 #define MAX_INPUT 24        // The max number specified by communication protocol I set up ie. 24 bytes encased by < >
 #define MAX_TIME 10000000   // ~2.6 hours before it auto-shuts down the pattern that's been holding
-#define TIMEOUT 3000        // 3 seconds before it no longer waits to receive the current pattern info
+#define TIMEOUT 2000        // 2 seconds before it no longer waits to receive the current pattern info
 
 
 CRGBArray<NUM_LEDS> leds;   // led array
-int input[MAX_INPUT];      // parse data one byte at a time
+byte input[MAX_INPUT];      // parse data one byte at a time
 unsigned long timeNoComm;   // long timeout if no communication is received
+unsigned int timeWaiting;   // short timeout for filling the instruction buffer
 bool validInput;
 
 // Explanation Notes
@@ -48,52 +49,75 @@ void loop() {
     Serial.println("Error: no communication timeout");
     // TODO set the currentPattern to be OFF or blank
   }
+  
 
-  /* Communication is either received as 24 bytes encased by <> or a communication receive error message
-   * is sent and instruction is disregarded */
-  if(Serial.available() > 0) {
-    if(Serial.available() == MAX_INPUT + 2) {
-      if(Serial.read() == 60) {    // '<' was read as first character
-        for(int i = 0; i < MAX_INPUT; ++i) {   // read 24 instruction parameter bytes into input array
-          input[i] = Serial.read();
-        }
-        if(Serial.read() == 62) { // '>' was read as last character
-          validInput = true;  // All parameters have been met for serial-read input; assume to be a valid instruction
-        } else {
-          Serial.println("Error: last delimiter");
-          clearBuffer();
-        }
-      } else {
-        Serial.println("Error: first delimiter");
-        clearBuffer();
-      }
-    } else { // Something is in the buffer but it's not the right length, clear buffer
-      Serial.println("Error: wrong buffer length");
-      clearBuffer(); 
-    }
-  }
-
-  if(validInput) {
+  if(readPattern()) {
     Serial.println("Status: received possibly valid input");
-    validInput = false;
     setupPattern();
   }
+}
 
 
+/* Handle serial input by monitoring the available buffer and reading it in when a full instruction is received */
+bool readPattern() {
+  /* Communication is either received as 24 bytes encased by <> or a communication receive error message
+   * is sent and instruction is disregarded */
+   
+  if(Serial.available() > 0) {
+    timeWaiting = millis();
+    clearInputArray();
+    
+    while(Serial.available() != MAX_INPUT + 2) {  // wait for full pattern instruction within timeout bounds
+      if((millis() - timeWaiting) > TIMEOUT) {
+        Serial.println("Error: pattern wait timeout");
+        clearBuffer();
+        return false;
+      }
+    }
+    /* At this point the full instruction is in the available buffer so test first delimiter */
+    
+    if((char) Serial.read() != '<') {
+      Serial.println("Error: first delimiter");
+      clearBuffer();
+      return false;
+    }
+    /* At this point the first character is the proper < delimiter so read in the available buffer values */
+
+    for(int i = 0; i < MAX_INPUT; ++i) {   // read 24 instruction parameter bytes into input array
+      input[i] = Serial.read();
+    }
+    /* At this point the characters have been read into the input array so test final delimiter*/
+   
+    if((char) Serial.read() != '>') {
+      Serial.println("Error: last delimiter");
+      clearBuffer();
+      return false;
+    }
+    /* At this point the final character is the proper > delimiter so assume everything is good to go */
+    return true;
+  }
+  return false;
 }
 
 
 /* Function to parse the entire pattern buffer, and set up the global pattern object */
-void setupPattern() {
-  switch(input[0]) {
-    case 1:
-      fill_solid(leds, NUM_LEDS, CRGB::Red);
-      FastLED.show();
-      break;
-    case 0:
+// TODO turn into a boolean similar to readPattern()
+void setupPattern() {  
+  switch((char) input[0]) {
+    case '0':
+      Serial.println("Status: matches off pattern");
       FastLED.clear();
       FastLED.show();
       break;
+      
+    case '1':
+      Serial.println("Status: matches solid pattern");
+      fill_solid(leds, NUM_LEDS, CRGB::Red);
+      FastLED.show();
+      break;
+
+    default:
+      Serial.println("Error: no pattern index found");
   }
 }
 
@@ -102,7 +126,7 @@ void setupPattern() {
 void clearInputArray() {
   Serial.println("Private: cleared input array");
   for(int i = 0; i < MAX_INPUT; ++i) {
-    input[i] = 45;
+    input[i] = byte('-');
   }
 }
 
